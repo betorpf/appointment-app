d0c75f4162475959accf93750cf0147a7777d995
Roberto Passareti Filho <betorpf@gmail.com>
2024-01-26 23:06:34 -0300

    Finalizado

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css'],
+})
+export class AppointmentListComponent implements OnInit {
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem('appointments');
+
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [] ;
+  }
+
+  appointment: Appointment = {
+    id: 1,
+    title: 'Take dog for walk',
+    date: new Date('2024-01-24'),
+  };
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+      };
+      this.appointments.push(newAppointment);
+
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+
+      // alert(this.appointments.length);
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
+    }
+  }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
+  }
+}
